77c9c5bceb773cb1ad96dd23a59fff5f0a0f8b5f
Alexander Kiel <alexanderkiel@gmx.net>
2014-04-09 20:45:39 +0200

    Initial Commit

--- src/logic_prog/core.clj
+++ src/logic_prog/core.clj
@@ -1,6 +1,20 @@
-(ns logic-prog.core)
+(ns logic-prog.core
+  (:require [clojure.test :refer (deftest are run-tests)]
+            [clojure.core.logic :refer (run == conde)]
+            [clojure.core.logic.fd :as fd])
+  (:refer-clojure :exclude [==]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn play1 [res vs]
+  (if-let [solution
+           (first
+            (run 1 [op x y]
+                 (fd/in x (apply fd/domain vs))
+                 (fd/in y (apply fd/domain vs))
+                 (fd/distinct [x y])
+                 (conde
+                  [(fd/+ x y res) (== op '+)]
+                  [(fd/- x y res) (== op '-)]
+                  [(fd/* x y res) (== op '*)])))]
+    (apply list solution)))
+
+(play1 1 [1 2])
